f47696eadb9195a4bf8c46c20fab456dd157cce1
pevic sanch <sanchez.pv@pm.me>
2024-07-28 20:47:14 +0200

    adding new dependencies

--- downloader.py
+++ downloader.py
@@ -19,6 +19,9 @@ def download_vimeo_audio(video_url, output_path):
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
             }],
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
+            }
         }
 
         # Download the audio from the video
